3c16fe58a76a0721d83b1735fdddc8b0ec977904
omqxze <joker9986@naver.com>
2021-04-06 14:23:36 +0900

    정훈

--- app/src/main/java/com/tester/apptest1/MainHome.java
+++ app/src/main/java/com/tester/apptest1/MainHome.java
@@ -1,14 +1,14 @@
-package com.example.myapplication;
+package com.tester.apptest1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_home);
     }
 }
